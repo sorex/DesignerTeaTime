871f99d0779344f9a190f73b25993fa716fd7464
sorex <sorex1984@gmail.com>
2013-01-21 10:04:29 +0800

    将引用集添加到git仓库中了

--- src/DesignerTeaTime/J.MainWeb/App_Start/BundleConfig.cs
+++ src/DesignerTeaTime/J.MainWeb/App_Start/BundleConfig.cs
@@ -8,8 +8,8 @@ namespace J.MainWeb
 		// 有关 Bundling 的详细信息，请访问 http://go.microsoft.com/fwlink/?LinkId=254725
 		public static void RegisterBundles(BundleCollection bundles)
 		{
-			bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-						"~/Scripts/jquery-{version}.js"));
+			bundles.Add(new ScriptBundle("~/Static/jquery/files").Include(
+						"~/Static/jquery/jquery-{version}.js"));
 
 			bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
 						"~/Scripts/jquery-ui-{version}.js"));
