1c87f94da4e212c3c4a64c827e9f332671e22635
sorex <sorex1984@gmail.com>
2013-01-21 17:51:31 +0800

    添加temp系列页面

--- src/DesignerTeaTime/J.MainWeb/App_Start/BundleConfig.cs
+++ src/DesignerTeaTime/J.MainWeb/App_Start/BundleConfig.cs
@@ -11,33 +11,29 @@ namespace J.MainWeb
 			bundles.Add(new ScriptBundle("~/Static/jquery/files").Include(
 						"~/Static/jquery/jquery-{version}.js"));
 
-			bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
-						"~/Scripts/jquery-ui-{version}.js"));
+			bundles.Add(new ScriptBundle("~/Static/jquery-ui/js/files").Include(
+						"~/Static/jquery-ui/js/jquery-ui-{version}.js"));
 
-			bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-						"~/Scripts/jquery.unobtrusive*",
-						"~/Scripts/jquery.validate*"));
+			bundles.Add(new ScriptBundle("~/Static/jquery.validate/files").Include(
+						"~/Static/jquery.validate/jquery.validate*"));
 
 			// 使用 Modernizr 的开发版本进行开发和了解信息。然后，当你做好
 			// 生产准备时，请使用 http://modernizr.com 上的生成工具来仅选择所需的测试。
-			bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-						"~/Scripts/modernizr-*"));
+			bundles.Add(new ScriptBundle("~/Static/modernizr/files").Include(
+						"~/Static/modernizr/modernizr-*"));
 
-			bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
+			bundles.Add(new StyleBundle("~/Static/css").Include("~/Static/site.css"));
 
-			bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
-						"~/Content/themes/base/jquery.ui.core.css",
-						"~/Content/themes/base/jquery.ui.resizable.css",
-						"~/Content/themes/base/jquery.ui.selectable.css",
-						"~/Content/themes/base/jquery.ui.accordion.css",
-						"~/Content/themes/base/jquery.ui.autocomplete.css",
-						"~/Content/themes/base/jquery.ui.button.css",
-						"~/Content/themes/base/jquery.ui.dialog.css",
-						"~/Content/themes/base/jquery.ui.slider.css",
-						"~/Content/themes/base/jquery.ui.tabs.css",
-						"~/Content/themes/base/jquery.ui.datepicker.css",
-						"~/Content/themes/base/jquery.ui.progressbar.css",
-						"~/Content/themes/base/jquery.ui.theme.css"));
+			bundles.Add(new StyleBundle("~/Static/jquery-ui/css/files").Include(
+						"~/Static/jquery-ui/css/jquery-ui*"));
+
+
+			bundles.Add(new ScriptBundle("~/Static/bootstrap/js/files").Include(
+						"~/Static/bootstrap/js/bootstrap*"));
+
+			bundles.Add(new StyleBundle("~/Static/bootstrap/css/files").Include(
+						"~/Static/bootstrap/css/bootstrap.css",
+						"~/Static/bootstrap/css/bootstrap-responsive.css"));
 		}
 	}
 }
---
+++ src/DesignerTeaTime/J.MainWeb/Controllers/TempController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace J.MainWeb.Controllers
+{
+    public class TempController : Controller
+    {
+        //
+        // GET: /Temp/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+		public ActionResult JQueryUI()
+		{
+			return View();
+		}
+    }
+}
