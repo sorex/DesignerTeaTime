712482ea401e162e0b31268c9efd69e6d3d33cfd
sorex <sorex1984@gmail.com>
2013-01-23 18:02:40 +0800

    添加Nlog的引用

--- src/DesignerTeaTime/J.MainWeb/Controllers/HomeController.cs
+++ src/DesignerTeaTime/J.MainWeb/Controllers/HomeController.cs
@@ -4,12 +4,18 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
+using NLog;
+
 namespace J.MainWeb.Controllers
 {
 	public class HomeController : Controller
 	{
+		private static Logger logger = LogManager.GetCurrentClassLogger();
+
 		public ActionResult Index()
 		{
+			logger.Log(LogLevel.Debug, "this is a testA");
+			logger.Log(LogLevel.Debug, "this is a testB");
 			return View();
 		}
 	}
