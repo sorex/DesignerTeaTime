46d99ca278affce2ec6c24474be572a755ba7f14
sorex <sorex1984@gmail.com>
2013-01-31 16:05:27 +0800

    添加Home/UploadFile以配合uploadify控件，同时添加上传文件夹和日志文件夹的忽略

--- src/DesignerTeaTime/J.MainWeb/Controllers/HomeController.cs
+++ src/DesignerTeaTime/J.MainWeb/Controllers/HomeController.cs
@@ -5,6 +5,8 @@ using System.Web;
 using System.Web.Mvc;
 
 using NLog;
+using System.IO;
+using Newtonsoft.Json;
 
 namespace J.MainWeb.Controllers
 {
@@ -18,5 +20,31 @@ namespace J.MainWeb.Controllers
 			//logger.Log(LogLevel.Debug, "this is a testB");
 			return View();
 		}
+
+		[HttpPost]
+		public ActionResult UploadFile()
+		{
+			HttpPostedFileBase file = Request.Files["Filedata"]; //获取单独文件的访问
+			var fileGuid = Guid.NewGuid().ToString();//生成随机的guid
+			try
+			{
+				if (file != null)
+				{
+					var uploadPath = Server.MapPath("~/Files") + "/Temp/" + fileGuid;
+					if (!Directory.Exists(uploadPath))
+					{ //判断上传的文件夹是否存在 
+						Directory.CreateDirectory(uploadPath);
+					}
+					file.SaveAs(uploadPath + '/' + file.FileName);
+					return Content(JsonConvert.SerializeObject(new { state = "success", msg = fileGuid }));
+				}
+				return Content(JsonConvert.SerializeObject(new { state = "error", msg = "文件不存在，请重新上传！" }));
+			}
+			catch (Exception e)
+			{
+				return Content(JsonConvert.SerializeObject(new { state = "error", msg = e.Message }));
+			}
+		}
+
 	}
 }
