d7715d00bbbd013e0c79ce6340e7d7ee6568ef6c
sorex <sorex1984@gmail.com>
2013-01-31 16:01:39 +0800

    添加jquery UI的汉化包，json2.js，uploadify 3.1
    修改了BundleConfig文件和_Layout.cshtml以完成上述变更

--- src/DesignerTeaTime/J.MainWeb/App_Start/BundleConfig.cs
+++ src/DesignerTeaTime/J.MainWeb/App_Start/BundleConfig.cs
@@ -8,33 +8,69 @@ namespace J.MainWeb
 		// 有关 Bundling 的详细信息，请访问 http://go.microsoft.com/fwlink/?LinkId=254725
 		public static void RegisterBundles(BundleCollection bundles)
 		{
-			bundles.Add(new ScriptBundle("~/Static/jquery/files").Include(
-						"~/Static/jquery/jquery-{version}.js"));
 
-			bundles.Add(new ScriptBundle("~/Static/jquery-ui/js/files").Include(
-						"~/Static/jquery-ui/js/jquery-ui-{version}.js"));
+			#region CSS
+			#region Site CSS
+			bundles.Add(new StyleBundle("~/Static/css").Include(
+						"~/Static/reset.css",
+						"~/Static/site.css"));
+			#endregion
 
-			bundles.Add(new ScriptBundle("~/Static/jquery.validate/files").Include(
-						"~/Static/jquery.validate/jquery.validate*"));
+			#region jquery ui CSS
+			bundles.Add(new StyleBundle("~/Static/jquery-ui/css/files").Include(
+						"~/Static/jquery-ui/css/jquery-ui*"));
+			#endregion
 
+			#region uploadify CSS
+			bundles.Add(new StyleBundle("~/Static/uploadify/css").Include(
+			"~/Static/uploadify/uploadify.css"));
+			#endregion
+			#endregion
+
+			#region JS
+			#region Modernizr
 			// 使用 Modernizr 的开发版本进行开发和了解信息。然后，当你做好
 			// 生产准备时，请使用 http://modernizr.com 上的生成工具来仅选择所需的测试。
 			bundles.Add(new ScriptBundle("~/Static/modernizr/files").Include(
 						"~/Static/modernizr/modernizr-*"));
+			#endregion
 
-			bundles.Add(new StyleBundle("~/Static/css").Include(
-						"~/Static/reset.css",
-						"~/Static/site.css"));
+			#region jquery
+			bundles.Add(new ScriptBundle("~/Static/jquery/files").Include(
+						"~/Static/jquery/jquery-{version}.js"));
+			#endregion
 
-			bundles.Add(new StyleBundle("~/Static/jquery-ui/css/files").Include(
-						"~/Static/jquery-ui/css/jquery-ui*"));
+			#region jquery ui
+			bundles.Add(new ScriptBundle("~/Static/jquery-ui/js/files").Include(
+						"~/Static/jquery-ui/js/jquery-ui-{version}.js",
+						"~/Static/jquery-ui/js/i18n/jquery.ui.datepicker-zh-CN.js"));
+			#endregion
 
+			#region jquery validate
+			bundles.Add(new ScriptBundle("~/Static/jquery.validate/files").Include(
+						"~/Static/jquery.validate/jquery.validate*"));
+			#endregion
 
+			#region jquery layout
 			bundles.Add(new ScriptBundle("~/Static/jquery.layout/files").Include(
 						"~/Static/jquery.layout/jquery.layout*"));
+			#endregion
 
+			#region json2
+			bundles.Add(new ScriptBundle("~/Static/json2/files").Include(
+			"~/Static/json2/json2*"));
+			#endregion
+
+			#region uploadify
+			bundles.Add(new ScriptBundle("~/Static/uploadify/js").Include(
+						"~/Static/uploadify/jquery.uploadify-{version}.js"));
+			#endregion
+
+			#region extendjs
 			bundles.Add(new ScriptBundle("~/Static/extendjs/files").Include(
 						"~/Static/extendjs/extendjs*"));
+			#endregion
+			#endregion
 
 		}
 	}
---
+++ src/DesignerTeaTime/J.MainWeb/Static/uploadify/jquery.uploadify.localization-3.1.js
@@ -0,0 +1,65 @@
+﻿//*********************************************************************
+//1.查找如下代码，并注释掉alert行：
+//	case SWFUpload.UPLOAD_ERROR.UPLOAD_LIMIT_EXCEEDED:
+//	//alert('The upload limit has been reached (' + errorMsg + ').');
+//	errorString = 'Exceeds Upload Limit';
+//	break;
+//*********************************************************************
+
+//*********************************************************************
+//2.将汉化一下部分
+//	'Some files were not added to the queue:'
+//	'一些文件未被加入到队列：'
+//
+//	'\nThe number of files selected exceeds the remaining upload limit ('
+//	'\n所选文件的数量超过剩余的上传限制数量 ('
+//
+//	'\nThe number of files selected exceeds the queue size limit ('
+//	'\n所选文件的数量超过了队列大小限制 ('
+//
+//	'\nThe file "'
+//	'\n文件 "'
+//
+//	'" exceeds the size limit ('
+//	'" 超过大小限制 ('
+//
+//	'" is empty.'
+//	'" 为空文件.'
+//
+//	'" is not an accepted file type ('
+//	'" 不是允许的文件类型 ('
+//
+//	' - Cancelled'
+//	' - 取消'
+//
+//	' - Complete'
+//	' - 完成'
+//*********************************************************************
+
+//*********************************************************************
+//3.初始化时加入一下代码，用于处理弹出框，不加入则默认弹窗
+//	overrideEvents: ['onDialogClose'],
+//	onDialogClose: function (queueData)
+//	{
+//		//filesSelected		The number of files selected in browse files dialog
+//		//filesQueued		The number of files added to the queue (that didn’t return an error)
+//		//filesReplaced		The number of files replaced in the queue
+//		//filesCancelled	The number of files that were cancelled from being added to the queue (not replaced)
+//		//filesErrored		The number of files that returned an error
+//		if (queueData.filesErrored > 0)
+//		{
+//			alert(queueData.errorMsg);
+//		}
+//	},
+//*********************************************************************
+
+//*********************************************************************
+//4.适应本地的参数
+//	swf: '/Static/uploadify/uploadify.swf',		//'@Url.Content("~/Static/uploadify/uploadify.swf")'
+//	uploader: '/Home/UploadFile',				//'@Url.Action("UploadFile", "Home")'
+//	// Options
+//	buttonText: '选择文件',     // The text to use for the browse button
+//	fileTypeDesc: '所有文件',        // The description for file types in the browse dialog
+//	multi: false,               // Allow multiple file selection in the browse dialog
+//	fileSizeLimit: '4MB',
+//*********************************************************************
