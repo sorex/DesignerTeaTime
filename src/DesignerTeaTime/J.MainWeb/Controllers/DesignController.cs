82964506c0c5c28fc241f6a0780e4b18b7133ea3
sorex <sorex1984@gmail.com>
2013-02-20 22:56:01 +0800

    去掉了tinyMCE的autosave插件

--- src/DesignerTeaTime/J.MainWeb/Controllers/DesignController.cs
+++ src/DesignerTeaTime/J.MainWeb/Controllers/DesignController.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
+using Newtonsoft.Json;
+using J.Entities;
+
 namespace J.MainWeb.Controllers
 {
 	public class DesignController : Controller
@@ -13,7 +16,11 @@ namespace J.MainWeb.Controllers
 
 		public ActionResult Index()
 		{
-
+			using(DBEntities db = new DBEntities())
+			{
+				ViewBag.MaterialList = JsonConvert.SerializeObject((from m in db.Materials
+									   select new { GUID = m.GUID, Name = m.Name }).ToList());
+			}
 			return View();
 		}
 
