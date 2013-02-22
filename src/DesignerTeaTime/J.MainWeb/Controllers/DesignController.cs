30a6e7fe092b385e1bade3c738ca88deffe6b2d9
sorex <sorex1984@gmail.com>
2013-02-22 18:17:28 +0800

    提交

--- src/DesignerTeaTime/J.MainWeb/Controllers/DesignController.cs
+++ src/DesignerTeaTime/J.MainWeb/Controllers/DesignController.cs
@@ -16,10 +16,18 @@ namespace J.MainWeb.Controllers
 
 		public ActionResult Index()
 		{
-			using(DBEntities db = new DBEntities())
+			using (DBEntities db = new DBEntities())
 			{
-				ViewBag.MaterialList = JsonConvert.SerializeObject((from m in db.Materials
-									   select new { GUID = m.GUID, Name = m.Name }).ToList());
+				var Materials = (from m in db.Materials
+								 select new { GUID = m.GUID, Name = m.Name }).ToList();
+				string MaterialID = Materials.FirstOrDefault().GUID;
+				var MaterialColors = (from c in db.MaterialColors
+									  where c.MaterialID == MaterialID
+									  select new { ColorName = c.ColorName, ColorCode = c.ColorCode, IsSelected = c.IsDefault }).ToList();
+				var MaterialPictures = (from p in db.MaterialPictures
+										where p.MaterialID == MaterialID
+										select new { p.GUID, p.Index, p.Name, p.FileName, p.Width, p.Height, p.Top, p.Left, p.UploadWidth, p.UploadHeight }).OrderBy(p => p.Index).ToList();
+				ViewBag.Data = JsonConvert.SerializeObject(new { Materials, MaterialColors, MaterialPictures });
 			}
 			return View();
 		}
